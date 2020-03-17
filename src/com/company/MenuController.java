de823e04ddccb41bb3b5985feb0e54efee498d9b
bugmanbig <marseniym@gmail.com>
2020-03-17 11:17:38 +0300

    Added empty menu frame

---
+++ src/com/company/MenuController.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MenuController {
+    private MenuView menuView;
+
+    public void setMenuView(MenuView menuView) {
+        this.menuView = menuView;
+    }
+
+    public void start() {
+        menuView.create();
+    }
+}
---
+++ src/com/company/MenuView.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class MenuView {
+    public void create() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+    }
+}
